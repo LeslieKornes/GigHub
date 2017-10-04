0e4f74614de6de6ebf7789b99e810f5d8cb822c5
Leslie Kornes <Lesliekornes@gmail.com>
2017-10-04 00:52:26 -0400

    git is being silly

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,53 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            var gigFormViewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(gigFormViewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(GigFormViewModel gigFormViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                gigFormViewModel.Genres = _context.Genres.ToList();
+                return View("Create", gigFormViewModel);
+            }
+
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = gigFormViewModel.GetDateTime(),
+                GenreId = gigFormViewModel.Genre,
+                Venue = gigFormViewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
