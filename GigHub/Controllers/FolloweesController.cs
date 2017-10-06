6ab4cdb36831cfd1410cb66c673bfd3f9d95fa30
Leslie Kornes <Lesliekornes@gmail.com>
2017-10-06 01:09:15 -0400

    Added Followees Controller

---
+++ GigHub/Controllers/FolloweesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var artists = _context.Followings
+                .Where(x => x.FollowerId == userId)
+                .Select(x => x.Followee)
+                .ToList();
+
+            return View(artists);
+        }
+    }
+}
