8d74e4f76f42b5c03453b46b3c2748cbaaae2be4
Leslie Kornes <Lesliekornes@gmail.com>
2017-10-25 23:18:46 -0400

    converted json data returned from api to camelCase

--- GigHub/Controllers/Api/NotificationsController.cs
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -1,10 +1,10 @@
-﻿using GigHub.Models;
+﻿using AutoMapper;
+using GigHub.Models;
 using Microsoft.AspNet.Identity;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Http;
-using AutoMapper;
 
 namespace GigHub.Controllers.Api
 {
@@ -20,16 +20,11 @@ namespace GigHub.Controllers.Api
         public IEnumerable<NotificationDto> GetNewNotifications()
         {
             var userId = User.Identity.GetUserId();
-
             var notifications = _context.UserNotifications
                 .Where(un => un.UserId == userId)
                 .Select(un => un.Notification)
                 .Include(un => un.Gig.Artist)
                 .ToList();
-
-            Mapper.CreateMap<ApplicationUser, UserDto>();
-            Mapper.CreateMap<Gig, GigDto>();
-            Mapper.CreateMap<Notification, NotificationDto>();
 
             return notifications.Select(Mapper.Map<Notification, NotificationDto>);
         }
