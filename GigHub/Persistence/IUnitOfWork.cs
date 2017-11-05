aa94679a3f72a5978461faf9e9eba8188ac01c1d
Leslie Kornes <Lesliekornes@gmail.com>
2017-11-05 00:25:37 -0400

    Created IUnitOfWorkRepository

---
+++ GigHub/Persistence/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using GigHub.Repositories;
+
+namespace GigHub.Persistence
+{
+    public interface IUnitOfWork
+    {
+        IGigRepository Gigs { get; }
+        IGenreRepository Genres { get; }
+        IFollowingRepository Followings { get; }
+        IAttendanceRepository Attendances { get; set; }
+        void Complete();
+    }
+}
